bd1b6db7f8c5b26d5679d9060e9227ea0cbd907f
Ivan <iqsk81ad@gmail.com>
2016-06-01 00:52:16 +0300

    #init es6 syntax

--- index.js
+++ index.js
@@ -1,67 +1,93 @@
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
-import expect from 'expect';
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+exports.setup = setup;
+exports.tc = tc;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactAddonsTestUtils = require('react-addons-test-utils');
+
+var _reactAddonsTestUtils2 = _interopRequireDefault(_reactAddonsTestUtils);
+
+var _expect = require('expect');
+
+var _expect2 = _interopRequireDefault(_expect);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function convertObject(object) {
 
-  if (typeof object === 'object') {
+  if ((typeof object === 'undefined' ? 'undefined' : _typeof(object)) === 'object') {
     return JSON.stringify(object);
   }
 
   return object;
-
 }
 
-export function setup(Component, props) {
+function setup(Component, props) {
 
-  const renderer = TestUtils.createRenderer();
+  var renderer = _reactAddonsTestUtils2.default.createRenderer();
 
-  renderer.render(
-    <Component { ...props }>{ props.children }</Component>
-  );
+  renderer.render(_react2.default.createElement(
+    Component,
+    props,
+    props.children
+  ));
 
-  const output = renderer.getRenderOutput();
+  var output = renderer.getRenderOutput();
 
   return {
-    output,
-    props
+    output: output,
+    props: props
   };
-
 }
 
 // const defaultTags = ['a', 'address', 'abbr', 'acronym', 'article', 'aside', 'b', 'big', 'blockquote', 'br', 'button', 'caption', 'cite', 'code', 'col', 'del', 'dd', 'details', 'div', 'dl', 'dt', 'em', 'fieldset', 'figure', 'figcaption', 'font', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup', 'hr', 'i', 'img', 'ins', 'kbd', 'label', 'legend', 'li', 'menu', 'nav', 'p', 'pre', 'q', 's', 'span', 'section', 'strike', 'strong', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'u', 'ul', 'ol', 'var'];
 
-export function tc(element, type, props = {}, childsLength = 0) { // test component
+function tc(element, type) {
+  var props = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+  var childsLength = arguments.length <= 3 || arguments[3] === undefined ? 0 : arguments[3];
+  // test component
 
   try {
-    expect(element.type).toBe(type);
+    (0, _expect2.default)(element.type).toBe(type);
   } catch (e) {
-    throw new Error(`Type ${convertObject(element.type)} not equal with ${convertObject(type)}`);
+    throw new Error('Type ' + convertObject(element.type) + ' not equal with ' + convertObject(type));
   }
 
-  for (const key in props) {
+  for (var key in props) {
     if (props.hasOwnProperty(key)) {
 
-      if (props[key] === 'test_off') { // if propery have some trobule like a react component
+      if (props[key] === 'test_off') {
+        // if propery have some trobule like a react component
         continue;
       }
 
       try {
-        expect(element.props[key]).toBe(props[key]);
+        (0, _expect2.default)(element.props[key]).toBe(props[key]);
       } catch (_) {
         try {
-          expect(element.props[key]).toEqual(props[key]);
+          (0, _expect2.default)(element.props[key]).toEqual(props[key]);
         } catch (e) {
-          throw new Error(`Property ${key} not equal Expected: ${convertObject(element.props[key])} To be: ${convertObject(props[key])}`);
+          throw new Error('Property ' + key + ' not equal Expected: ' + convertObject(element.props[key]) + ' To be: ' + convertObject(props[key]));
         }
       }
-
     }
   }
 
   if (childsLength !== null) {
 
-    let length = 1;
+    var length = 1;
 
     if (!element.props.children || element.props.children.length === 0) {
       length = 0;
@@ -70,20 +96,17 @@ export function tc(element, type, props = {}, childsLength = 0) { // test compon
     }
 
     if (length !== childsLength) {
-      throw new Error(`Children length not correct: Expected - ${length} not equal with ${childsLength}`);
+      throw new Error('Children length not correct: Expected - ' + length + ' not equal with ' + childsLength);
     }
-
   }
 
   if (typeof type === 'string') {
 
-    const elementPropsKeys = Object.keys({ ...element.props, children: undefined });
-    const propsKeys = Object.keys({ ...props, children: undefined });
+    var elementPropsKeys = Object.keys(_extends({}, element.props, { children: undefined }));
+    var propsKeys = Object.keys(_extends({}, props, { children: undefined }));
 
     if (elementPropsKeys.length !== propsKeys.length) {
-      throw new Error(`Property count not correct: Expected - ${elementPropsKeys.length} | ${convertObject(elementPropsKeys)} not equal with ${propsKeys.length} | ${convertObject(propsKeys)} (with children property by default)`);
+      throw new Error('Property count not correct: Expected - ' + elementPropsKeys.length + ' | ' + convertObject(elementPropsKeys) + ' not equal with ' + propsKeys.length + ' | ' + convertObject(propsKeys) + ' (with children property by default)');
     }
-
   }
-
 }
